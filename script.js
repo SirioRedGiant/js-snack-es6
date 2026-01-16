340a7eac53bbdabe171a928e2a4c00904006c468
SirioRedGiant <Oiris1992@gmail.com>
2026-01-16 17:07:50 +0100

    snack 1: Found the lightest bike

--- script.js
+++ script.js
@@ -47,3 +47,26 @@ const racingBikes = [
     weight: "6.8",
   },
 ];
+
+//note definisco una variabile con valore infinito ed una variabile vuota per la bici più leggera
+let min = Infinity;
+let lessWeightBike = null;
+
+console.log(min);
+
+for (let i = 0; i < racingBikes.length; i++) {
+  // confronto il valore infinito con il primo elemento del ciclo. Ad ogni ciclo ci sarà il confronto, fino a trovare il valore minimo nell'array
+  console.log(racingBikes[i].weight);
+  if (racingBikes[i].weight < min) {
+    console.log(racingBikes[i].name, "<-----");
+    min = racingBikes[i].weight; // qui si aggiorna se troviamo un valore inferiore
+    lessWeightBike = racingBikes[i]; // qui la bicicletta prende la chiave della bicicletta più leggera
+    console.log(lessWeightBike, "e lei");
+  }
+  console.log(lessWeightBike, "e lei");
+}
+console.log(lessWeightBike, "e lei");
+
+console.log(`The lighter bike is: ${lessWeightBike.name}`);
+
+
