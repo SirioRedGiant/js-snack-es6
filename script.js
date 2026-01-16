8faa5ccfc40a1358cc368068323ab75592a5844e
SirioRedGiant <Oiris1992@gmail.com>
2026-01-16 15:22:56 +0100

    array racingBike defined

--- script.js
+++ script.js
@@ -4,3 +4,46 @@
     Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
     Stampare a schermo la bici con peso minore.
 */
+
+const racingBikes = [
+  {
+    name: "Bianchi",
+    weight: "7.0",
+  },
+  {
+    name: "Look",
+    weight: "7.2",
+  },
+  {
+    name: "Willier Triestina",
+    weight: "7.5",
+  },
+  {
+    name: "Pinarello",
+    weight: "9.0",
+  },
+  {
+    name: "Cornago",
+    weight: "8.3",
+  },
+  {
+    name: "Cervelo s5",
+    weight: "7.8",
+  },
+  {
+    name: "Canyon",
+    weight: "9.2",
+  },
+  {
+    name: "Merida Reacto Team",
+    weight: "8.5",
+  },
+  {
+    name: "Specialized",
+    weight: "7.4",
+  },
+  {
+    name: "Trek",
+    weight: "6.8",
+  },
+];
